6475b5247a84d62d5f0645dd9883327e09a417e1
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-12-14 15:22:30 +0200

    Add email tag {download_list} without download links.

--- includes/EDD_Feedback_Emails.php
+++ includes/EDD_Feedback_Emails.php
@@ -37,7 +37,7 @@ class EDD_Feedback_Emails {
 		$email_to = $user_email;
 
 		$feedback = edd_feedback_get_feedback( $feedback_id );
-		$message  = ! empty( $feedback['message'] ) ? $feedback['message'] : __( "Hello {name},\n\nWe would love to hear feedback from you.\n\nYou have purchased following products from us: {edd_feedback_downloads}\n\nAre you happy with your purchase? Is there anything we can do for you?", "edd-feedback" );
+		$message  = ! empty( $feedback['message'] ) ? $feedback['message'] : __( "Hello {name},\n\nWe would love to hear feedback from you.\n\nYou have purchased following products from us:\n\n{downloads}\n\nAre you happy with your purchase? Is there anything we can do for you?", "edd-feedback" );
 		$message  = $this->filter_feedback_template_tags( $message, $user_email, $payment_id );
 		
 		// run our message through the standard EDD email tag function
@@ -72,34 +72,48 @@ class EDD_Feedback_Emails {
 
 	public function filter_feedback_template_tags( $text = '', $user_email = null, $payment_id = 0 ) {
 		
-		$user_id   = edd_get_payment_user_id( $payment_id );
-		$user_info = edd_get_payment_meta_user_info( $payment_id );
-		
-		// Retrieve the customer name
-		if ( $user_id ) {
-			$user_data     = get_userdata( $user_id );
-			$customer_name = $user_data->display_name;
-		} elseif ( isset( $user_info['first_name'] ) ) {
-			$customer_name = $user_info['first_name'];
-		} else {
-			$customer_name = $user_info['email'];
-		}
-		
-		// Get downloads
-		$downloads = edd_get_payment_meta_downloads( $payment_id );
-	
-		if ( is_array( $downloads ) ) {
-			// Create a list of downloads
-			$download_list = '';
-			$download_list = '<ul>';
-			foreach ( $downloads as $download ) {
-				$download_list = '<li>' . $download['name'] . '</li>';
+		// Get downloads in a list without links
+		$cart_items    = edd_get_payment_meta_cart_details( $payment_id );
+		$download_list = '<ul>';
+
+		if ( $cart_items ) {
+			foreach ( $cart_items as $item ) {
+
+				if ( edd_use_skus() ) {
+					$sku = edd_get_download_sku( $item['id'] );
+				}
+				
+				if ( edd_item_quantities_enabled() ) {
+					$quantity = $item['quantity'];
+				}
+
+				$price_id = edd_get_cart_item_price_id( $item );
+
+				$title = get_the_title( $item['id'] );
+				
+				// Add quantity to title
+				if ( ! empty( $quantity ) && $quantity > 1 ) {
+					$title .= "&nbsp;&ndash;&nbsp;" . __( 'Quantity', 'edd-feedback' ) . ': ' . $quantity;
+				}
+
+				// Add sku to title
+				if ( ! empty( $sku ) ) {
+					$title .= "&nbsp;&ndash;&nbsp;" . __( 'SKU', 'edd-feedback' ) . ': ' . $sku;
+				}
+
+				// Add variable pricing option to title
+				if ( $price_id !== null ) {
+					$title .= "&nbsp;&ndash;&nbsp;" . edd_get_price_option_name( $item['id'], $price_id, $payment_id );
+				}
+
+				$download_list .= '<li>' . apply_filters( 'edd_email_receipt_download_title', $title, $item, $price_id, $payment_id ) . '</li>';
+				
 			}
-			$download_list = '</ul>';
 		}
+
+		$download_list .= '</ul>';
 		
-		//$text = str_replace( '{name}', $customer_name, $text );
-		//$text = str_replace( '{downloads}', $downloads, $text );
+		$text = str_replace( '{downloads}', $download_list, $text );
 
 		return $text;
 	}
